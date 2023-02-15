d926a413e7d134e7d32f72d1016f7a402945f542
kangsunghyuk98 <kangsh9820@naver.com>
2023-02-15 11:33:47 +0900

    milky 부트 구조 생성

---
+++ src/main/java/com/example/milky/MilkyApplication.java
@@ -0,0 +1,13 @@
+package com.example.milky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilkyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MilkyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/milky/MilkyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.milky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
